fd54d7072afe6f982708db0e6de489467ce938f6
Deekshasen1 <121789944+Deekshasen1@users.noreply.github.com>
2023-07-30 16:15:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//   //1 .ways to print in java script
+//     // console.log("hello world");
+//     //alert("me");
+//     //document.write("this is document write")
+    
+//     //2. javascript consol api(application programming interface);
+//     console.log("hello world", 4+6, "another long");
+//     console.warn("this is warning");
+//     console.error("this is error");
+
+//datatypes
+//number
+var num=1;
+//string
+var str="hyy";
+//object work like key value paire
+var marks = {
+    ram : 1,
+    shyam : 2
+}
+//booleans
+var a = true;
+var b=false;
+ //var und = undefined;
+ var und;
+ console.log(und);
+
+ var n =null;
+ console.log(n);
+
+ /*
+ there are two types of datatypes
+ 1.primitive: undefined,null,number,string,boolean,symbol
+ 2. reference :array,object; */
